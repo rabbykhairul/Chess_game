2d89a5e743107dc03d37a668c7ef430951a38a37
rabbykhairul <rabbykhairul@gmail.com>
2019-10-04 19:22:56 +0600

    Fix typo in method name

--- board.rb
+++ board.rb
@@ -7,22 +7,22 @@ class Board
     end
 
     def place_chess_pieces_on_the_board_grid
-        place_blace_and_white_rooks_in_starting_position
-        place_blace_and_white_knights_in_starting_position
-        place_blace_and_white_bishops_in_starting_position
+        place_black_and_white_rooks_in_starting_position
+        place_black_and_white_knights_in_starting_position
+        place_black_and_white_bishops_in_starting_position
     end
 
-    def place_blace_and_white_bishops_in_starting_position
+    def place_black_and_white_bishops_in_starting_position
         bishop_cols = [ 2, 5 ]
         put_pieces(starting_rows, bishop_cols, Bishop)
     end
 
-    def place_blace_and_white_knights_in_starting_position
+    def place_black_and_white_knights_in_starting_position
         knight_cols = [ 1, 6 ]
         put_pieces(starting_rows, knight_cols, Knight)
     end
 
-    def place_blace_and_white_rooks_in_starting_position
+    def place_black_and_white_rooks_in_starting_position
         rook_cols = [ 0, 7 ]
         put_pieces(starting_rows, rook_cols, Rook)
     end
