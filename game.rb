5ab1b8be77f3145e2cef9c9c5b72fc235f95fc8f
rabbykhairul <rabbykhairul@gmail.com>
2019-10-07 23:08:42 +0600

    switch between players after each turn

--- game.rb
+++ game.rb
@@ -2,10 +2,14 @@ require_relative './board.rb'
 require_relative './player.rb'
 
 class Game
+    attr_reader :white_player, :black_player
+    attr_accessor :current_player
+
     def initialize
         @chess_board = Board.new
         @white_player = Player.new(:white)
         @black_player = Player.new(:black)
+        @current_player = @white_player
     end
 
     def start_game
@@ -13,6 +17,14 @@ class Game
             move_command = current_player.get_move
             make_move(move_command)
             switch_player!
+        end
+    end
+
+    def switch_player!
+        if self.current_player == self.white_player
+            self.current_player = self.black_player
+        else
+            self.current_player = self.white_player
         end
     end
 end
