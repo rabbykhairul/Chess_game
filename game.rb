cc263edcc40987d4dabcc12df142a5d42d160dad
rabbykhairul <rabbykhairul@gmail.com>
2019-10-07 22:57:39 +0600

    Add start_game method to start the game

--- game.rb
+++ game.rb
@@ -7,4 +7,12 @@ class Game
         @white_player = Player.new(:white)
         @black_player = Player.new(:black)
     end
+
+    def start_game
+        until checkmate?
+            move_command = current_player.get_move
+            make_move(move_command)
+            switch_player!
+        end
+    end
 end
