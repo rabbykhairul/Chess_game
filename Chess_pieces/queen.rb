9d515ce62284882726d1ce405b32af2712b2db76
rabbykhairul <rabbykhairul@gmail.com>
2019-10-10 07:50:10 +0600

    Find all possible positions a queen can move to from current position

--- Chess_pieces/queen.rb
+++ Chess_pieces/queen.rb
@@ -1,7 +1,22 @@
 require_relative "./chess_piece.rb"
+require_relative "./slideable_module.rb"
 
 class Queen < ChessPiece
+    include Slideable
+
     def symbol
         ' ♛ '.colorize(color)
+    end
+
+    def possible_moves
+        moves_in_horizontal_vertical_diagonal_direction
+    end
+
+    def moves_in_horizontal_vertical_diagonal_direction
+        find_moves(horizontal_vertical_diagonal_direction)
+    end
+
+    def horizontal_vertical_diagonal_direction
+        HORIZONTAL_DIRECTIONS + VERTICAL_DIRECTIONS + DIAGONAL_DIRECTIONS
     end
 end
