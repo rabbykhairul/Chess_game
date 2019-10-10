31c98f73387e1c41e739aed7e4f9bb109e1eae0a
rabbykhairul <rabbykhairul@gmail.com>
2019-10-10 13:37:12 +0600

    Refactor pawn class

--- Chess_pieces/pawn.rb
+++ Chess_pieces/pawn.rb
@@ -19,4 +19,28 @@ class Pawn < ChessPiece
     def forward_direction
         color == :black ? 1 : -1
     end
-end 
+
+    def possible_moves
+        moves = []
+
+        current_row, current_col = forward_direction + position[0], position[1]
+
+        if within_range?(current_row, current_col)
+            current_position = [ current_row, current_col ]
+            moves << current_position if can_move_to?(current_position)
+            
+            if at_starting_position? && !moves.empty?
+                current_row += forward_direction
+                current_position = [ current_row, current_col ]
+                moves << current_position if can_move_to?(current_position)
+            end
+        end
+
+        moves
+    end
+
+    def can_move_to?(position)
+        piece_at_position = board[ position ]
+        piece_at_position.is_a?(NullPiece)
+    end
+end
