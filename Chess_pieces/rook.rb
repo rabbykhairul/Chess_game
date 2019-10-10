b5b0413dc42a422c6eadb9cbfd6687c7eb1786d4
rabbykhairul <rabbykhairul@gmail.com>
2019-10-10 07:14:49 +0600

    Find all possible positions a rook can be moved to from current position

--- Chess_pieces/rook.rb
+++ Chess_pieces/rook.rb
@@ -1,7 +1,26 @@
 require_relative "./chess_piece.rb"
+require_relative "./slideable_module.rb"
 
 class Rook < ChessPiece
+    include Slideable
+
     def symbol
         ' ♜ '.colorize(color)
+    end
+
+    def possible_moves
+        moves_in_horizontal_and_vertical_direction
+    end
+
+    def moves_in_horizontal_and_vertical_direction
+        move_positions = []
+        horizontal_and_vertical_direction.each do |dir|
+            move_positions += grow_moves_in_direction(dir)
+        end
+        move_positions
+    end
+
+    def horizontal_and_vertical_direction
+        HORIZONTAL_DIRECTIONS + VERTICAL_DIRECTIONS
     end
 end
