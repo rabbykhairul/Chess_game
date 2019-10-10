641343e89e84a66f6ace17d9c301ff7bef03bd9b
rabbykhairul <rabbykhairul@gmail.com>
2019-10-10 11:58:50 +0600

    Find all positions a knight can move to from current position

--- Chess_pieces/knight.rb
+++ Chess_pieces/knight.rb
@@ -1,7 +1,34 @@
 require_relative "./chess_piece.rb"
 
 class Knight < ChessPiece
+    DIRECTIONS = [
+        [ 2, 1 ],
+        [ 2, -1 ],
+        [ -2, 1],
+        [ -2, -1],
+        [ -1, -2 ],
+        [ 1, -2 ],
+        [ -1, 2 ],
+        [ 1, 2 ]
+    ]
+
     def symbol
         ' ♞ '.colorize(color)
+    end
+
+    def possible_moves
+        moves = []
+        
+        DIRECTIONS.each do |dir|
+            current_row, current_col = dir[0] + position[0], dir[1] + position[1]
+
+            if within_range?(current_row, current_col)
+                current_pos = [ current_row, current_col ]
+                current_piece = board[ current_pos ]
+                moves << current_pos if current_piece.is_a?(NullPiece) || current_piece.color != self.color
+            end
+        end
+
+        moves
     end
 end
