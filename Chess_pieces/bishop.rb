124062d5ae0c5ee39e33f882b333f9bcbd471a5d
rabbykhairul <rabbykhairul@gmail.com>
2019-10-10 07:52:56 +0600

    Find all possible positions a bishop can move to from current position

--- Chess_pieces/bishop.rb
+++ Chess_pieces/bishop.rb
@@ -1,7 +1,18 @@
 require_relative "./chess_piece.rb"
+require_relative "./slideable_module.rb"
 
 class Bishop < ChessPiece
+    include Slideable
+
     def symbol
         ' ♝ '.colorize(color)
+    end
+
+    def possible_moves
+        find_moves(diagonal_diretion)
+    end
+
+    def diagonal_diretion
+        DIAGONAL_DIRCTIONS
     end
 end
