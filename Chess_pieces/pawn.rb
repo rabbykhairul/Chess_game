8c54373ea9e6fcd1ef81c6ee0b03950c82baec30
rabbykhairul <rabbykhairul@gmail.com>
2019-10-10 12:54:13 +0600

    Add start_position and move_direction attribute to pawn class

--- Chess_pieces/pawn.rb
+++ Chess_pieces/pawn.rb
@@ -1,7 +1,13 @@
 require_relative "./chess_piece.rb"
 
 class Pawn < ChessPiece
+    def initialize(color, position, board)
+        super
+        @start_position = position
+        @move_direction = @color == :black ? [1,0] : [-1,0]
+    end
+
     def symbol
         ' ♟ '.colorize(color)
     end
-end
+end 
