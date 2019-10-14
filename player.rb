3f9b83d5544893f044a912806c934ed8f95f9748
rabbykhairul <rabbykhairul@gmail.com>
2019-10-14 10:11:58 +0600

    Get move from player

--- player.rb
+++ player.rb
@@ -1,7 +1,28 @@
 class Player
-    attr_reader :color
+    attr_reader :color, :display
     
-    def initialize(color)
+    def initialize(color, display)
         @color = color
+        @display = display
+    end
+
+    def get_move
+        display_the_board
+        get_the_move_command
+    end
+
+    def display_the_board
+        system("clear")
+        puts "\t#{color}'s turn: "
+        puts "\t------"
+        display.render_board
+    end
+
+    def get_the_move_command
+        puts "Please select a chess piece to move(example: a8 or d1): "
+        piece_pos = gets.chomp
+        puts "Move to where?(example: b2 or c8)"
+        end_pos = gets.chomp
+        { :start_pos => piece_pos, :destination_pos => end_pos }
     end
 end
